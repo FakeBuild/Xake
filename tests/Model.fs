c6e6637f6bdb2cf460da51d49b262c55e436663e
Oleg Zee <olegzzzz@gmail.com>
2014-03-02 03:02:32 +0700

    init

---
+++ tests/Model.fs
@@ -0,0 +1,28 @@
+﻿namespace tests
+
+module Types =
+  open System.IO
+  open System.Threading.Tasks
+
+  type Rule = (unit) -> FileInfo
+
+  type Artifact =
+    | File of FileInfo
+    | Rule of Task<FileInfo>
+
+  type Deferred<'t>(a:Async<'t>) =
+    let mutable task:Task<'t> = null
+    member this.Wait =
+      if (task = null) then
+        task <- Async.StartAsTask(a)
+      Async.AwaitTask task
+    
+module Actions =
+
+  open System.IO
+
+  let FileSet pattern =
+    let dirIndo = DirectoryInfo (System.IO.Path.GetDirectoryName pattern)
+    let mask = System.IO.Path.GetFileName pattern
+    Seq.map Types.File (dirIndo.EnumerateFiles mask)
+
---
+++ tests/Script.fsx
@@ -0,0 +1,97 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Model.fs"
+open System.Threading.Tasks
+open System.IO
+
+open tests
+open tests.Types
+
+// Define your library scripting code here
+let files = Actions.FileSet "..\*.*"
+let names = Seq.map (fun (File f) -> f.Name) files |> Seq.toArray
+
+let log (s:string) =
+  System.Console.Out.WriteLine(s) |> ignore
+  System.Console.Out.Flush
+
+let task (a:Async<'t>) =
+  Rule (new Task<'t>(fun () -> (Async.RunSynchronously a)))
+
+//let task a = Async.StartAsTask(a)
+
+let wait = function
+  | File f -> Async.FromContinuations (fun (cont,_e,_c) -> cont(f))
+  | Rule r ->
+    r.Start()
+    (Async.AwaitTask r)
+
+let target0 = File (new FileInfo("c:\\!\\1"))
+
+let target2 = task(async {
+  log "making file2..."
+  do! Async.Sleep(3000)
+  log "done file2..."
+  return new FileInfo("c:\\!\\2")
+  })
+
+let target1 = task (async {
+  log "making file1..."
+  
+  do! Async.Sleep(1000)
+  log "done file1..."
+  return new FileInfo("c:\\!\\1")
+  })
+
+let target3 = task (async {
+  log "making file3..."
+  let! file1 = wait target1
+  let result = new FileInfo("c:\\!\\3")
+
+  File.WriteAllText (result.FullName, "==== file3 ====\r\n" + File.ReadAllText(file1.FullName) + "\r\n========")
+  do! Async.Sleep(2000)
+  log "done file3..."
+  return result
+  })
+
+let main = task (async {
+  log "start main"
+  let! file1 = wait target1
+  let! file2 = wait target2
+  let! file3 = wait target3
+
+  let text1 = File.ReadAllText(file1.FullName)
+  let text2 = File.ReadAllText(file2.FullName)
+  let text3 = File.ReadAllText(file3.FullName)
+
+  let file3 = new FileInfo("c:\\!\main.c")
+  File.WriteAllText (file3.FullName, "file1\r\n" + text1 + "\n\rfile2\r\n" + text2 + "\r\nfile3\r\n" + text3)
+
+  return file3
+  })
+
+wait main
+//let compileTask = task (fun () ->
+//  Control.Async.Sleep 1000 |> ignore
+//  new FileInfo("c:\\1"))
+//
+
+
+let t1 = new System.Threading.Tasks.Task<int>(fun () -> 
+  printf "running task"
+  123)
+
+t1.AsyncState
+t1.Result
+t1.Start
+Async.AwaitTask t1
+
+let a = async {
+  do! Async.Sleep(100)
+  printf "a task"
+  return 123
+  }
+
+let t11 = Async.StartAsTask(a)
+Async.AwaitTask t11
