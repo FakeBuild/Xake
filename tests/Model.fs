dabbd651ff058a8c30515614bce2d4407ac4db12
Oleg Zee <olegzzzz@gmail.com>
2014-03-05 02:51:17 +0700

    prototype 1

--- tests/Model.fs
+++ tests/Model.fs
@@ -2,27 +2,28 @@
 
 module Types =
   open System.IO
+
+  type Rule =
+    | File
+    | Build of Async<unit>
+
+  type Artifact = Artifact of FileInfo * Rule
+  type FileSet = Files of Artifact list
+
   open System.Threading.Tasks
-
-  type Rule = (unit) -> FileInfo
-
-  type Artifact =
-    | File of FileInfo
-    | Rule of Async<FileInfo>
-
   type Deferred<'t>(a:Async<'t>) =
     let mutable task:Task<'t> = null
     member this.Wait =
       if (task = null) then
         task <- Async.StartAsTask(a)
       Async.AwaitTask task
-    
+
 module Actions =
 
+  open Types
   open System.IO
 
-  let FileSet pattern =
-    let dirIndo = DirectoryInfo (System.IO.Path.GetDirectoryName pattern)
-    let mask = System.IO.Path.GetFileName pattern
-    Seq.map Types.File (dirIndo.EnumerateFiles mask)
-
+  let fileset pattern =
+    let dirIndo = DirectoryInfo (Path.GetDirectoryName pattern)
+    let mask = Path.GetFileName pattern
+    FileSet.Files (Seq.map (fun f -> Artifact (f,Types.Rule.File)) (dirIndo.EnumerateFiles mask) |> List.ofSeq)
--- tests/Script.fsx
+++ tests/Script.fsx
@@ -9,96 +9,88 @@ open tests
 open tests.Types
 
 // Define your library scripting code here
-let files = Actions.FileSet "..\*.*"
-let names = Seq.map (fun (File f) -> f.Name) files |> Seq.toArray
+let files = Actions.fileset "..\*.*"
+
+let (Files ff) = files
+let names = List.map (fun (Artifact (f,_)) -> f.Name) ff |> List.toArray
 
 let log (s:string) =
   System.Console.Out.WriteLine(s) |> ignore
-  System.Console.Out.Flush
+  System.Console.Out.Flush |> ignore
 
-let task a = Rule a
-
-//let task a = Async.StartAsTask(a)
-
-let wait = function
-  | File f -> Async.FromContinuations (fun (cont,_e,_c) -> cont(f))
-  | Rule r -> r
-
-let target0 = File (new FileInfo("c:\\!\\1"))
-
-let target2 = Rule (async {
-  log "making file2..."
-  do! Async.Sleep(3000)
-  log "done file2..."
-  return new FileInfo("c:\\!\\2")
-  })
-
-let target1 = Rule (async {
-  log "making file1..."
+let run (Artifact (file,rule)) =
+  match rule with
+  | File -> Async.FromContinuations (fun (cont,_e,_c) -> cont(file))
+  | Build r -> async {
+    do! r
+    return file
+  }
+let runMany = Seq.ofArray >> Seq.map run >> Async.Parallel 
   
-  do! Async.Sleep(1000)
-  log "done file1..."
-  return new FileInfo("c:\\!\\1")
-  })
+// execution context
+let mutable context = Map.empty
 
-let target3 = Rule (async {
+let exec (Artifact (file,rule)) =
+  let task = context |> Map.tryFind file.FullName
+
+  match task,rule with
+  | Some task, _->
+    async {
+      do! Async.AwaitTask task
+      return file
+    }
+  | None,File -> Async.FromContinuations (fun (cont,_,_) -> cont(file))
+  | None,Build r ->
+    let task = Async.StartAsTask r
+    context <- Map.add file.FullName task context
+    log (">>added task " + file.FullName + " from thread#" + System.Threading.Thread.CurrentThread.ManagedThreadId.ToString())
+    async {
+      do! Async.AwaitTask task
+      return file
+    }
+
+let execMany = Seq.ofArray >> Seq.map exec >> Async.Parallel 
+
+let fileinfo path = new FileInfo(path)
+let simplefile path = Artifact (fileinfo path,File)
+let (<<<) path steps = Artifact (fileinfo path,Build steps)
+
+let target0 = simplefile "c:\\!\\1"
+
+let target2 = "c:\\!\\2" <<< async {
+  do log "making file2..."
+  do! Async.Sleep(3000)
+  log "done file2..." |> ignore
+  }
+
+let target1 = "c:\\!\\1" <<< async {
+  log "making file1..."  
+  do! Async.Sleep(3000)
+  log "done file1..."
+  }
+
+let target3 = "c:\\!\\3" <<< async {
   log "making file3..."
-  let! file1 = wait target1
-  let result = new FileInfo("c:\\!\\3")
+  let! file1 = exec target1
+  let result = fileinfo "c:\\!\\3"
 
   File.WriteAllText (result.FullName, "==== file3 ====\r\n" + File.ReadAllText(file1.FullName) + "\r\n========")
   do! Async.Sleep(2000)
   log "done file3..."
-  return result
-  })
+  }
 
-let main = Rule (async {
+let main = "c:\\!\main.c" <<< async {
   log "start main"
-  let! file1 = wait target1
-  let! file2 = wait target2
-  let! file3 = wait target3
+  context <- Map.empty
+  let! [|file1;file2;file3|] = execMany [|target1; target2; target3|]
 
   let text1 = File.ReadAllText(file1.FullName)
   let text2 = File.ReadAllText(file2.FullName)
   let text3 = File.ReadAllText(file3.FullName)
 
-  let file3 = new FileInfo("c:\\!\main.c")
+  let file3 = fileinfo "c:\\!\main.c"
   File.WriteAllText (file3.FullName, "file1\r\n" + text1 + "\n\rfile2\r\n" + text2 + "\r\nfile3\r\n" + text3)
-
-  return file3
-  })
-
-Async.RunSynchronously(wait main)
-//let compileTask = task (fun () ->
-//  Control.Async.Sleep 1000 |> ignore
-//  new FileInfo("c:\\1"))
-//
-
-
-let t1 = new System.Threading.Tasks.Task<int>(fun () -> 
-  printf "running task"
-  123)
-
-t1.AsyncState
-t1.Result
-t1.Start
-Async.AwaitTask t1
-
-let file = wait (File (new FileInfo("c:\\!\\1")))
-let a = async {
-  do! Async.Sleep(100)
-  printf "a task"
-  return 123
   }
 
-let b = async {
-  let! ares = a
-  let! file = file
-  do! Async.Sleep(100)
-  printf "b task"
-  return 123
-  }
-
-Async.RunSynchronously b
-
-
+log (">>running from thread#" + System.Threading.Thread.CurrentThread.ManagedThreadId.ToString())
+Async.RunSynchronously(run main)
