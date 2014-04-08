7d0a60e2554516dadfeb4fab89d43d0f8ea6164f
Oleg Zee <olegzzzz@gmail.com>
2014-04-09 01:35:36 +0700

    draft implementation of task pool

---
+++ Research/Tasks.fsx
@@ -0,0 +1,14 @@
+﻿module Tasks =
+
+  open System
+  open System.Threading.Tasks
+
+  let t = new Task<string> (Func<string> (fun () -> "Hello"))
+  let onComplete = async {
+    printfn "waiting task"
+    let! a = Async.AwaitTask t
+    printfn "task complete with result: %A" a
+  }
+
+  Async.Start onComplete
+  t.Start()
--- core/Core.fs
+++ core/Core.fs
@@ -8,43 +8,82 @@ module Core =
 
   open Xake
 
+  // task pool
+  let MaxThreads = 5
+
+  type TaskPoolMessage =
+    | RunTask of Async<unit> * AsyncReplyChannel<Task<unit>>
+    | TaskComplete
+    | TaskSuspend of bool
+
+    // TODO resume with awaiting the pool is free
+
+  let taskPool = MailboxProcessor.Start(fun mbox ->
+    
+    let wrapTask a = async {
+      try
+        do! a
+      finally
+        mbox.Post TaskComplete
+    }
+
+    let rec loop x p =
+      //log Info "loop %A [%A]" x (List.length p)
+      if x >= MaxThreads || not (List.isEmpty p) then wait x p else run x p
+
+    and run taskCount pool = (async {
+      let! msg = mbox.Receive()
+      match msg with
+      | RunTask (task,chnl) ->
+        chnl.Reply(Async.StartAsTask (wrapTask task))
+        return! loop (taskCount + 1) pool
+      | TaskComplete ->
+        return! loop (taskCount - 1) pool
+      | TaskSuspend true ->
+        return! loop (taskCount - 1) pool
+      | TaskSuspend false ->
+        return! loop (taskCount + 1) pool
+    })
+    and wait taskCount pool = (async {
+      let! msg = mbox.Receive()
+      match msg with
+      | RunTask (task,chnl) ->
+        let t = new Task<unit> (fun() -> Async.RunSynchronously (wrapTask task))
+        chnl.Reply(t)
+        return! loop taskCount (pool @ [t]) // TODO use queue
+      | TaskComplete
+      | TaskSuspend true ->
+        match pool with
+        |t::ts -> 
+          t.Start()
+          return! loop taskCount ts
+        | [] ->
+          return! loop (taskCount-1) pool
+      | TaskSuspend false ->
+        return! loop (taskCount + 1) pool
+    })
+    loop 0 [] )
+
   // execution context
   type ExecMessage =
-    | Run of Artifact * BuildAction * AsyncReplyChannel<Task<FileInfo>>
-    | Reset
-    | GetTask of FileInfo * AsyncReplyChannel<Task<FileInfo> option>
+    | Run of Artifact * BuildAction * AsyncReplyChannel<Async<unit>>
 
-  let execstate = MailboxProcessor.Start(fun mbox ->
+  let actionPool = MailboxProcessor.Start(fun mbox ->
     let rec loop(map) = async {
       let! msg = mbox.Receive()
       match msg with
-      | Reset ->        
-        // TODO cancel pending tasks
-        // map |> Map.iter (fun file task -> ...)
-        return! loop(Map.empty)
-
       | Run(artifact, BuildAction action, chnl) ->
-        
-        let fullname = fullname artifact
-
-        match map |> Map.tryFind fullname with
+        match map |> Map.tryFind artifact.FullName with
         | Some task ->
           log Verbose "Task found for '%s'. Waiting for completion" artifact.Name
-          chnl.Reply(task)
+          chnl.Reply(Async.AwaitTask task)
           return! loop(map)
 
         | None ->
-          let task = Async.StartAsTask (async {
-            do! action artifact
-            return artifact
-          })
           log Verbose "Starting new task for '%s'" artifact.Name
-          chnl.Reply(task)
-          return! loop(map |> Map.add fullname task)
-
-      | GetTask(file,chnl) ->        
-        chnl.Reply (map |> Map.tryFind file.FullName)
-        return! loop(map)
+          let! task = taskPool.PostAndAsyncReply(fun chnl -> RunTask(action artifact, chnl))
+          chnl.Reply(Async.AwaitTask task)
+          return! loop(map |> Map.add artifact.FullName task)
     }
     loop(Map.empty) )
 
@@ -54,7 +93,7 @@ module Core =
     //exit errorCode
 
   // TODO how does it work?
-  execstate.Error.Add(fun e -> exitWithError 1 e.Message e)
+  actionPool.Error.Add(fun e -> exitWithError 1 e.Message e)
 
   // TODO make a parameter
   let projectRoot = Directory.GetCurrentDirectory()
@@ -86,15 +125,19 @@ module Core =
     match locateRule artifact with
     | Some rule ->
       async {
-        let! task = execstate.PostAndAsyncReply(fun chnl -> Run(artifact, rule, chnl))
-        return! Async.AwaitTask task
+        let! task = actionPool.PostAndAsyncReply(fun chnl -> Run(artifact, rule, chnl))
+        return! task
       }
     | None ->
       if not artifact.Exists then exitWithError 2 (sprintf "Neither rule nor file is found for '%s'" (fullname artifact)) ""
-      Async.FromContinuations (fun (cont,_,_) -> cont(artifact))
+      Async.FromContinuations (fun (cont,_,_) -> cont())
 
   let exec = Seq.ofList >> Seq.map execOne >> Async.Parallel
-  let need = getFiles >> exec >> Async.Ignore
+  let need x = async {
+    do taskPool.Post (TaskSuspend true)
+    do! x |> (getFiles >> exec >> Async.Ignore)
+    do taskPool.Post (TaskSuspend false)
+    }
 
   /// Runs execution of all artifact rules in parallel
   let run targets =
