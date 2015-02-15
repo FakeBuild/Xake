978b604771ade877c0c94a7ab488764bcca827e9
olegz <olegzzzz@gmail.com>
2015-02-16 01:38:12 +0600

    automatic version increment

---
+++ core/AssemblyInfo.fs
@@ -0,0 +1,10 @@
+﻿module XakeLib.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("Xake core")>]
+[<assembly: AssemblyDescription("Xake core library")>]
+[<assembly: AssemblyCopyright("(c) OlegZee 2014-1015")>]
+[<assembly: AssemblyVersion(XakeConst.Version)>]
+
+()
---
+++ core/VersionInfo.fs
@@ -0,0 +1,1 @@
+﻿module XakeConst [<Literal>]  let internal Version = "0.1.0.0"
