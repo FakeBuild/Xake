f4c472c27e013e6256b2e3c6360b74b8e4271786
olegzee <olegzzzz@gmail.com>
2015-02-17 02:26:56 +0600

    -

---
+++ samples/bootstrap-build.fsx
@@ -0,0 +1,28 @@
+// boostrapping xake.core
+System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let file = System.IO.Path.Combine("packages", "Xake.Core.dll")
+if not (System.IO.File.Exists file) then
+    printf "downloading xake.core assembly..."; System.IO.Directory.CreateDirectory("packages") |> ignore
+    let url = "https://github.com/OlegZee/Xake/releases/download/v0.1.2/Xake.Core.dll"
+    use wc = new System.Net.WebClient() in wc.DownloadFile(url, file + "__"); System.IO.File.Move(file + "__", file)
+    printfn ""
+
+// xake build file body
+#r @"packages/Xake.Core.dll"
+
+open Xake
+
+do xake {XakeOptions with FileLog = "build.log"; Threads = 4 } {
+
+  rule ("main" ==> ["helloworld.exe"])
+
+  rule("*.exe" *> fun exe -> action {
+    do! Csc {
+      CscSettings with
+        Out = exe
+        Src = !! (exe.Name -. "cs")
+      }
+    })
+
+}
