3bfccdb8ca92d0ad3ddccbee4f9d1d8bd9fd1e0c
olegz <olegzzzz@gmail.com>
2016-10-09 21:30:33 +0700

    pushing to nuget

--- build.fsx
+++ build.fsx
@@ -16,6 +16,33 @@ open Xake
 open Xake.SystemTasks
 
 let TestsAssembly = "bin/XakeLibTests.dll"
+let (=?) value deflt = match value with |Some v -> v |None -> deflt
+
+let DEF_VER = "0.0.1"
+
+let nuget_exe args = system (useClr >> checkErrorLevel) "packages/NuGet.CommandLine/tools/NuGet.exe" args |> Action.Ignore
+
+module nuget =
+    module private impl =
+        let newline = System.Environment.NewLine
+        let wrapXml node value = sprintf "<%s>%s</%s>" node value node
+        let wrapXmlNl node (value:string) =
+            let attrs = value.Split([|newline|], System.StringSplitOptions.None) |> Seq.ofArray
+            let content = attrs |> Seq.map ((+) "  ") |> String.concat newline
+            sprintf "<%s>%s</%s>" node (newline + content + newline) node
+        let toXmlStr (node,value) = wrapXml node value
+
+    open impl
+
+    let dependencies deps =
+        "dependencies", newline
+        + (deps |> List.map (fun (s,v) -> sprintf """<dependency id="%s" version="%s" />""" s v) |> String.concat newline)
+        + newline
+
+    let metadata = List.map toXmlStr >> String.concat newline >> wrapXmlNl "metadata"
+    let files = List.map (fun(f,t) -> (f,t) ||> sprintf """<file src="%s" target="%s" />""") >> String.concat newline >> wrapXmlNl "files"
+    let target t ff = ff |> List.map (fun file -> file,t)
+    let package = String.concat newline >> wrapXmlNl "package" >> ((+) ("<?xml version=\"1.0\"?>" + newline))
 
 do xake {ExecOptions.Default with ConLogLevel = Verbosity.Chatty } {
     var "NETFX-TARGET" "4.5"
@@ -134,4 +161,51 @@ do xake {ExecOptions.Default with ConLogLevel = Verbosity.Chatty } {
 
         }
     ]
+
+    (* Nuget publishing rules *)
+    rules [
+        "nuget-pack" => action {
+
+            let libFiles = ["bin/Xake.Core.dll"]
+            do! need libFiles
+
+            let! ver = getEnv("VER")
+
+            let nuspec =
+                nuget.package [
+                    nuget.metadata [
+                        "id", "Xake"
+                        "version", ver =? DEF_VER
+                        "authors", "OlegZee"
+                        "owners", "OlegZee"
+                        "projectUrl", "https://github.com/OlegZee/Xake"
+                        "requireLicenseAcceptance", "false"
+                        "description", "Xake build tool"
+                        "releaseNotes", ""
+                        "copyright", sprintf "Copyright %i" System.DateTime.Now.Year
+                        "tags", "Xake F# Build"
+                        nuget.dependencies []
+                    ]
+                    nuget.files (libFiles |> nuget.target "tools")
+                ]
+
+            let nuspec_file = "_.nuspec"
+            do System.IO.Directory.CreateDirectory("nupkg") |> ignore
+            do System.IO.File.WriteAllText(nuspec_file, nuspec)
+
+            do! nuget_exe ["pack"; nuspec_file; "-OutputDirectory"; "nupkg" ]
+        }
+
+        "nuget-push" => action {
+
+            do! need ["nuget-pack"]
+
+            let! ver = getEnv("VER")
+            let package_name = sprintf "Xake.%s.nupkg" (ver =? DEF_VER)
+
+            let! nuget_key = getEnv("NUGET_KEY")
+            do! nuget_exe ["push"; "nupkg" </> package_name; nuget_key =? ""; "-Source"; "nuget.org"]
+        }
+    ]
 }
+
