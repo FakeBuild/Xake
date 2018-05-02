4ad5da3992d486a86250010c0fb145ce0917ce95
Oleg Zaimkin <olegzzzz@gmail.com>
2018-05-02 17:58:45 +0700

    netcore/FAKE 5 bootstrap example

---
+++ samples/netcore-bootstrap/fake.cmd
@@ -0,0 +1,5 @@
+REM Install .NET Core (https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script)
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Channel Current"
+REM SET PATH=%LOCALAPPDATA%\Microsoft\dotnet;%PATH%
+dotnet restore dotnet-fake.csproj
+dotnet fake %*
