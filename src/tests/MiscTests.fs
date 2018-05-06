f0cf71dc908bd31767ef706578588a845f37f5a3
Oleg Zaimkin <olegzzzz@gmail.com>
2018-05-07 00:32:49 +0700

    reverted RecipeTests and MiscTests

--- src/tests/MiscTests.fs
+++ src/tests/MiscTests.fs
@@ -1,4 +1,4 @@
-﻿module ``Various tests``
+﻿namespace Tests
 
 open System.IO
 open NUnit.Framework
@@ -7,74 +7,53 @@ open Xake
 open Xake.Tasks
 open Xake.Tasks.Dotnet
 
-let private rememberDir = Directory.GetCurrentDirectory()
-let private outFolder = rememberDir </> "~testout~" </> "misc"
+type ``Various tests``() =
+    inherit XakeBaseTest("misc")
 
-[<OneTimeSetUp>]
-let setup () =
-    Directory.CreateDirectory outFolder |> ignore
-    Directory.SetCurrentDirectory outFolder
+    let taskReturn n = recipe {
+        return n
+    }
 
-[<OneTimeTearDown>]
-let teardown () =
-    Directory.SetCurrentDirectory rememberDir
+    [<Test>]
+    member x.``runs csc task (full test)``() =
 
-[<SetUp>]
-let setupTest () =
-    try File.Delete("." </> ".xake") with _ -> ()
+        let needExecuteCount = ref 0
+        
+        do xake {x.TestOptions with FileLog="skipbuild.log"} {  // one thread to avoid simultaneous access to 'wasExecuted'
+            want (["hello"])
 
-let xakeOptions = { ExecOptions.Default with ProjectRoot = outFolder }
+            rules [
+                "hello" ..> recipe {
+                    do! trace Error "Running inside 'hello' rule"
+                    do! need ["hello.cs"]
 
-[<Test>]
-let ``runs csc task (full test)``() =
-
-    let needExecuteCount = ref 0
-    
-    do xake {xakeOptions with Threads = 1; FileLog="skipbuild.log"} {  // one thread to avoid simultaneous access to 'wasExecuted'
-        want (["hello"])
-
-        rules [
-            "hello" ..> recipe {
-                do! trace Error "Running inside 'hello' rule"
-                do! need ["hello.cs"]
-
-                do! trace Error "Rebuilding..."
-                do! Csc {
-                CscSettings with
-                    Src = !!"hello.cs"
+                    do! trace Error "Rebuilding..."
+                    do! Csc {
+                    CscSettings with
+                        Src = !!"hello.cs"
+                    }
                 }
-            }
-            "hello.cs" ..> action {
-                do! writeText """class Program
-                {
-	                public static void Main()
-	                {
-		                System.Console.WriteLine("Hello world!");
-	                }
-                }"""
-                let! src = getTargetFullName()
-                do! trace Error "Done building 'hello.cs' rule in %A" src
-                needExecuteCount := !needExecuteCount + 1
-            }
-        ]
-    }
+                "hello.cs" ..> action {
+                    do! writeText """class Program
+                    {
+    	                public static void Main()
+    	                {
+    		                System.Console.WriteLine("Hello world!");
+    	                }
+                    }"""
+                    let! src = getTargetFullName()
+                    do! trace Error "Done building 'hello.cs' rule in %A" src
+                    needExecuteCount := !needExecuteCount + 1
+                }
+            ]
+        }
 
-    Assert.AreEqual(1, !needExecuteCount)
+        Assert.AreEqual(1, !needExecuteCount)
 
-[<Test>]
-let ``resource set instantiation``() =
+    [<Test>]
+    member x.``resource set instantiation``() =
 
-    let resset = resourceset {
-        prefix "Sample.Application"
-        dynamic true
-
-        files (fileset {
-            includes "*.resx"
-        })
-    }
-
-    let resourceSetCollection = [
-        resourceset {
+        let resset = resourceset {
             prefix "Sample.Application"
             dynamic true
 
@@ -82,101 +61,107 @@ let ``resource set instantiation``() =
                 includes "*.resx"
             })
         }
-        resourceset {
-            prefix "Sample.Application1"
-            dynamic true
 
-            files (fileset {
-                includes "*.res"
-            })
+        let resourceSetCollection = [
+            resourceset {
+                prefix "Sample.Application"
+                dynamic true
+
+                files (fileset {
+                    includes "*.resx"
+                })
+            }
+            resourceset {
+                prefix "Sample.Application1"
+                dynamic true
+
+                files (fileset {
+                    includes "*.res"
+                })
+            }
+        ]
+
+        printfn "%A" resset
+        ()
+
+    #if false // !NETCOREAPP2_0
+    // this test requires different assembly refs for netcore, and also different way to start
+    // I suppose it should be another test for netcore/FAKE 5 env
+
+    // TODO fix the path to Xake assembly
+
+    [<Test>]
+    member x.``script exits with errorlevel on script failure``() =
+
+        let fsiApp = if Xake.Env.isRunningOnMono then "fsharpi" else "fsi"
+        let errorCode = ref 0
+        System.IO.Directory.CreateDirectory("1") |> ignore
+        
+        do xake {x.TestOptions with FileLog="exits-with-errorlevel.log"; FileLogLevel = Verbosity.Diag; Targets = ["one"] } {
+            rules [
+                "one" => recipe {
+                    do! need ["1/script.fsx"]
+                    let! ec = shell {cmd fsiApp; args ["1/script.fsx"]}
+                    errorCode := ec
+                }
+                "1/script.fsx" ..> writeText """
+                    #r "../../../bin/Debug/net46/Xake.dll"
+                    open Xake
+
+                    do xake {ExecOptions.Default with DbFileName=".1err"; Threads = 4 } {
+
+                    phony "main" (action {
+                        do! trace Message "Hello world!"
+                        failwith "error-text"
+                        })
+
+                    }"""
+            ]
         }
-    ]
 
-    printfn "%A" resset
-    ()
+        Assert.AreEqual(2, !errorCode)
+    #endif
 
-#if false // !NETCOREAPP2_0
-// this test requires different assembly refs for netcore, and also different way to start
-// I suppose it should be another test for netcore/FAKE 5 env
+    [<Test>]
+    member x.``failif is a short circuit for task result``() =
 
-// TODO fix the path to Xake assembly
+        let excCount = ref 0
+        do xake {x.TestOptions with FileLog="failf.log"} {
+            rules [
+                "main" => (recipe {
+                    do! taskReturn 3 |> FailWhen ((=) 3) "err" |> Recipe.Ignore
+                } |> WhenError (fun _ -> excCount := 1))
+            ]
+        }
 
-[<Test>]
-let ``script exits with errorlevel on script failure``() =
+        Assert.AreEqual(1, !excCount)
 
-    let fsiApp = if Xake.Env.isRunningOnMono then "fsharpi" else "fsi"
-    let errorCode = ref 0
-    System.IO.Directory.CreateDirectory("1") |> ignore
-    
-    do xake {xakeOptions with Threads = 1; FileLog="exits-with-errorlevel.log"; FileLogLevel = Verbosity.Diag; Targets = ["one"] } {
-        rules [
-            "one" => recipe {
-                do! need ["1/script.fsx"]
-                let! ec = shell {cmd fsiApp; args ["1/script.fsx"]}
-                errorCode := ec
-            }
-            "1/script.fsx" ..> writeText """
-                #r "../../../bin/Debug/net46/Xake.dll"
-                open Xake
+    [<Test>]
+    member x.``WhenError handler intercepts the error``() =
 
-                do xake {ExecOptions.Default with DbFileName=".1err"; Threads = 4 } {
+        let ex = ref 0
 
-                phony "main" (action {
-                    do! trace Message "Hello world!"
-                    failwith "error-text"
-                    })
+        // pipe result, and provide fallback value in case of error
+        do xake {x.TestOptions with FileLog="failf.log"} {
+            rules [
+                "main" => recipe {
+                    do! taskReturn 3
+                        |> FailWhen ((=) 3) "fail"
+                        |> WhenError (fun _ -> ex := 1; 0)
+                        |> Recipe.Ignore
+                }
+            ]
+        }
+        // intercept error for resultless action
+        do xake {x.TestOptions with FileLog="failf2.log"} {
+            rules [
+                "main" => recipe {
+                    do! taskReturn 3
+                        |> FailWhen ((=) 3) "fail"
+                        |> Recipe.Ignore
+                        |> WhenError (fun _ -> ex := !ex + 1)
+                }
+            ]
+        }
 
-                }"""
-        ]
-    }
-
-    Assert.AreEqual(2, !errorCode)
-#endif
-
-let taskReturn n = recipe {
-    return n
-}
-
-[<Test>]
-let ``failif is a short circuit for task result``() =
-
-    let excCount = ref 0
-    do xake {xakeOptions with Threads = 1; FileLog="failf.log"} {
-        rules [
-            "main" => (recipe {
-                do! taskReturn 3 |> FailWhen ((=) 3) "err" |> Recipe.Ignore
-            } |> WhenError (fun _ -> excCount := 1))
-        ]
-    }
-
-    Assert.AreEqual(1, !excCount)
-
-[<Test>]
-let ``WhenError handler intercepts the error``() =
-
-    let ex = ref 0
-
-    // pipe result, and provide fallback value in case of error
-    do xake {xakeOptions with Threads = 1; FileLog="failf.log"} {
-        rules [
-            "main" => recipe {
-                do! taskReturn 3
-                    |> FailWhen ((=) 3) "fail"
-                    |> WhenError (fun _ -> ex := 1; 0)
-                    |> Recipe.Ignore
-            }
-        ]
-    }
-    // intercept error for resultless action
-    do xake {xakeOptions with Threads = 1; FileLog="failf2.log"} {
-        rules [
-            "main" => recipe {
-                do! taskReturn 3
-                    |> FailWhen ((=) 3) "fail"
-                    |> Recipe.Ignore
-                    |> WhenError (fun _ -> ex := !ex + 1)
-            }
-        ]
-    }
-
-    Assert.AreEqual(2, !ex)
+        Assert.AreEqual(2, !ex)
